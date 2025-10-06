6c29c190a7342fd7ed41d1595c05ec1c91bc9d7e
mirerakepha <kephamirera16@gmail.com>
2025-10-06 09:46:34 +0300

    partialreloadmessages scrollToBottom remove typed text after send

---
+++ a_rtchat/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import Model
+
+
+class ChatGroup(models.Model):
+    group_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.group_name
+
+
+
+class GroupMessage(models.Model):
+    group = models.ForeignKey(ChatGroup, on_delete=models.CASCADE, related_name='chat_messages')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField(max_length=300)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author.username} : {self.body}'
+
+
+    class Meta:
+        ordering = ['created']

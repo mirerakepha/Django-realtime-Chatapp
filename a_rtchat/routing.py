2f1dd8c998568b4df1ffe599cad821890dfc9f7c
mirerakepha <kephamirera16@gmail.com>
2025-10-08 08:08:26 +0300

    add online status

--- a_rtchat/routing.py
+++ a_rtchat/routing.py
@@ -1,6 +1,8 @@
-from django.urls import re_path
-from .consumers import ChatroomConsumer
+
+from django.urls import re_path, path
+from .consumers import ChatroomConsumer, OnlineStatusConsumer
 
 websocket_urlpatterns = [
     re_path(r"^ws/chatroom/(?P<chatroom_name>[^/]+)/$", ChatroomConsumer.as_asgi()),
+    path("ws/online-status/", OnlineStatusConsumer.as_asgi()),
 ]
